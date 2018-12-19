525cf4c8aa202a08ef3d7b7e7620b036254501b6
gokayumutlu <gokay.umutlu.353@outlook.com>
2018-12-20 01:39:19 +0300

    another commit

--- app/src/main/java/com/gokay/bitirmeprojesi/M/URL.java
+++ app/src/main/java/com/gokay/bitirmeprojesi/M/URL.java
@@ -2,8 +2,13 @@ package com.gokay.bitirmeprojesi.M;
 
 public class URL {
     private String baseUrl="https://bitirme-projesi-219000.appspot.com/";
+    private String mBaseUrl="http://10.0.2.2:3000/";
 
     public String getBaseUrl() {
         return baseUrl;
+    }
+
+    public String getmBaseUrl(){
+        return mBaseUrl;
     }
 }
